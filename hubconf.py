faf8cba09ef55bb1901e63e91859925bc412a254
Stefan Bachhofner <stefan.bachhofner@wu.ac.at>
2019-09-06 14:22:16 +0200

    Added new entrypoints

--- hubconf.py
+++ hubconf.py
@@ -1,3 +1,6 @@
 dependencies = ['torch']
 
-from models import MinkUNet34C
+
+from models.point_cloud_models import coordinates, coordinates_epoch,\
+                                      coordinates_colors, coordinates_colors_epoch,\
+                                      get_minkunet34c
