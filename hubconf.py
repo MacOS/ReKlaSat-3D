f80c8f279a37beb17fff060b3890c7e499820f05
Stefan Bachhofner <stefan.bachhofner@wu.ac.at>
2020-01-30 21:46:17 +0100

    Added weighted experiment endpoints to hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -3,4 +3,5 @@ dependencies = ['torch']
 
 from models.point_cloud_models import coordinates, coordinates_epoch,\
                                       coordinates_colors, coordinates_colors_epoch,\
+                                      coordinates_colors_weighted, coordinates_colors_weighted_epoch,\
                                       get_minkunet34c
